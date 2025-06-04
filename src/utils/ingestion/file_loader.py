ce5ab11e98ac389790cebe4a75be245261f47ee5
Maryam Syeda <maryamsyeda@mac.lan>
2025-06-04 15:40:31 -0700

    read me

--- src/utils/ingestion/file_loader.py
+++ src/utils/ingestion/file_loader.py
@@ -1,18 +1,4 @@
 import os
-
-# def get_all_file_paths(folder_path : str):
-
-#     if not os.path.isdir(folder_path):
-#         raise ValueError(f"The path '{folder_path}' is not a valid directory.")
-
-#     pptx_files = [
-#         os.path.join(folder_path, file)
-#         for file in os.listdir(folder_path)
-#         if file.lower().endswith('.pptx') and os.path.isfile(os.path.join(folder_path, file))
-#     ]
-
-#     return pptx_files
-
 from pathlib import Path
 
 def get_all_file_paths(folder_path: str):
